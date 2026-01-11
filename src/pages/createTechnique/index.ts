4a0428326709f5d32d08d5a2f440e72c43cb4158
ethand <ethandamian@ciencias.unam.mx>
2026-01-11 17:41:26 -0600

    feature: add root variables for css

---
+++ src/pages/createTechnique/index.ts
@@ -0,0 +1,1 @@
+export { CreateTechniquePage } from "./CreateTechniquePage";
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export * from "./myTechniques";
+export * from "./createTechnique";
+export * from "./reviewTechniques";
